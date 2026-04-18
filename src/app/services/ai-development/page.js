ed5b46df6c2f5c9dbfad689905c081bab2b5a503
niranjan <niranjan@7unique.in>
2026-04-18 16:46:04 +0530

    updated changes

--- src/app/services/ai-development/page.js
+++ src/app/services/ai-development/page.js
@@ -1,11 +1,13 @@
 import AIServiceHeroSection from "@/components/services/ai-development/AIServiceHeroSection";
 import TrustedClientsStrip from "@/components/Services/ai-development/TrustedClientsStrip";
+import AllUseCasesSection from "@/components/Services/ai-development/AIUseCasesSection";
 
 export default function Page() {
   return (
     <>
       <AIServiceHeroSection />
       <TrustedClientsStrip />
+      <AllUseCasesSection />
     </>
   );
 }
