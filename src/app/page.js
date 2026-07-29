6d8dc8286676504d362722a562082a30f21e4d94
DevikaChhipa <chhipadevika0@gmail.com>
2026-07-29 10:45:28 +0530

    jj

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
      <ProcessTimelineSection />  
      {/* <BlogInsights /> */}
      <CTABand />
-     <ServicesClientSection />
+     {/* <ServicesClientSection /> */}
     </div>
   );
 }
